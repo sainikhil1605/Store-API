6717549ff673258553cc09cb40ca1afe3d6c7bf7
Nikhil Reddy Vatti <35322884+sainikhil1605@users.noreply.github.com>
2021-12-07 17:05:40 +0530

    Added sorting and pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,10 @@
 const Product = require('../models/products');
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({}).sort('-name price');
   res.status(200).json(products);
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, select, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured;
@@ -15,7 +15,42 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  }
+  if (select) {
+    const selectList = select.split(',').join(' ');
+    result = result.select(selectList);
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      '<': '$lt',
+      '<=': '$lte',
+      '>': '$gt',
+      '>=': '$gte',
+      ':': '$eq',
+    };
+    const regex = /\b(<|>|>=|=|<|<=)\b/g;
+    const filters = numericFilters.replace(
+      regex,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ['price', 'rating'];
+    filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+  console.log(queryObject);
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  const products = await result;
   res.status(200).json({ products, Hits: products.length });
 };
 module.exports = { getAllProductsStatic, getAllProducts };
